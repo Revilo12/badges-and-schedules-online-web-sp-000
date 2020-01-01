6b9ff2ae1586c7d8b595eb492b11e806b9bc0a28
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-01 23:06:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,6 @@
 # Write your code here.
 def badge_maker(name)
-  print "Hello, my name is #{name}."
-  return "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array_of_names)
