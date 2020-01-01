5e6ae7e9861cd68280139536a061e1bf6f1fe2cc
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-01 22:51:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,6 @@ def batch_badge_creator(array_of_names)
   array_of_names.collect { |name| badge_maker(name)}
 end
 
+def assign_rooms(array_of_names)
+  array_of_names.each_with_index.collect do |name, index| 
+    
