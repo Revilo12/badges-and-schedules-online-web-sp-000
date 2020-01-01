641269973aa7cf3fea002fe7b35a89e4d65872bf
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-01 22:51:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,4 +10,7 @@ end
 
 def assign_rooms(array_of_names)
   array_of_names.each_with_index.collect do |name, index| 
-    
+    puts "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    return "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
